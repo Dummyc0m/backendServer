8f678a82380fe204f7a5065acfeb87561c0dabb9
Codetector <codetector@codetector.cn>
2017-01-08 16:09:20 -0500

    md-icon now accepts font-awesome in slot

---
+++ src/main/java/cn/com/guardiantech/classroom/server/data/quiz/QuizService.kt
@@ -0,0 +1,18 @@
+package cn.com.guardiantech.classroom.server.data.quiz
+
+import cn.com.guardiantech.classroom.server.data.AbstractDataService
+
+object QuizService: AbstractDataService() {
+    override fun initialize() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saveToDatabase(action: () -> Unit) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadFromDatabase(action: () -> Unit) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
